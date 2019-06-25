8abc4a1d9937305d43d6ed82c195cf6de07c9121
dave2000sang <david2000sang@hotmail.com>
2019-06-24 22:01:45 -0400

    wip contours trying to find license plate

--- detect_region.py
+++ detect_region.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def resize_img(img, width=500.0):
 	cur_width = img.shape[1]
@@ -19,3 +20,22 @@ cv2.imwrite('processed2.jpg', img_resized)
 img_bilateral = cv2.bilateralFilter(img_resized, 15, 30, 30)
 cv2.imwrite('bilateral.jpg', img_bilateral)
 
+img_edges = cv2.Canny(img_bilateral, 100, 200)
+cv2.imwrite('edges.jpg', img_edges)
+
+contours, hierarchy = cv2.findContours(img_edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+# iterate through contours
+
+for contour in contours:
+	perimeter = cv2.arcLength(contour, True)
+	approx = cv2.approxPolyDP(contour, perimeter * 0.02, True)
+	if len(approx) == 4:
+		plate_region = approx
+
+		print(plate_region)
+		img_copy = img_resized.copy()
+		img_copy = cv2.cvtColor(img_copy, cv2.COLOR_GRAY2RGB)
+		cv2.drawContours(img_copy, [plate_region], -1, (255,0,0), 3)
+		cv2.imshow('img with contours', img_copy)
+		cv2.waitKey(0)
