ad13ca6f03aa50dd33654bc095dc13e4c71f76cb
dave2000sang <david2000sang@hotmail.com>
2019-07-06 16:19:24 -0400

    working version

--- detect_region.py
+++ detect_region.py
@@ -9,19 +9,20 @@ def resize_img(img, width=500.0):
 	return new_width, new_height
 
 
+IMG_WIDTH = 1300.0
 GRAYSCALE = 0
 img = cv2.imread('test-plate.jpg', GRAYSCALE)
 
-w, h = resize_img(img, 1000.0)
+w, h = resize_img(img, IMG_WIDTH)
 img_resized = cv2.resize(img, dsize=(w, h), interpolation=cv2.INTER_LANCZOS4)
 
-cv2.imwrite('processed2.jpg', img_resized)
+# cv2.imwrite('processed2.jpg', img_resized)
 
 img_bilateral = cv2.bilateralFilter(img_resized, 15, 30, 30)
-cv2.imwrite('bilateral.jpg', img_bilateral)
+# cv2.imwrite('bilateral.jpg', img_bilateral)
 
 img_edges = cv2.Canny(img_bilateral, 100, 200)
-cv2.imwrite('edges.jpg', img_edges)
+# cv2.imwrite('edges.jpg', img_edges)
 
 contours, hierarchy = cv2.findContours(img_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -32,20 +33,29 @@ rectangles = []
 
 for contour in contours:
 	perimeter = cv2.arcLength(contour, True)
-	approx = cv2.approxPolyDP(contour, perimeter * 0.01, True)
+	# approx = contour
+	approx = cv2.approxPolyDP(contour, perimeter * 0.02, True)
 	if len(approx) >= 4 and perimeter > 100:
 		plate_region = approx
-		possible_plates.append(plate_region)
-		min_rect = cv2.minAreaRect(approx)
-		box = cv2.boxPoints(min_rect)
-		box = np.int0(box)
-		rectangles.append(box)
-		
+		min_rect = cv2.minAreaRect(plate_region)
+		angle = min_rect[2]
+		# print(min_rect)
+		width_to_height_ratio = min_rect[1][0]/min_rect[1][1]
+		area = min_rect[1][0] * min_rect[1][1]
+
+		if abs(angle) < 1 and width_to_height_ratio > 1.8 and area > (IMG_WIDTH/10)**2:
+			print(min_rect)
+			box = np.int0(cv2.boxPoints(min_rect))
+			possible_plates.append(plate_region)
+			rectangles.append(box)
 
 print(len(possible_plates))
 img_copy = img_resized.copy()
 img_copy = cv2.cvtColor(img_copy, cv2.COLOR_GRAY2RGB)
-cv2.drawContours(img_copy, possible_plates, -1, (255,0,0), 2)
-cv2.drawContours(img_copy, rectangles, -1, (0,255,0), 2)
+cv2.drawContours(img_copy, possible_plates, -1, (0,0,255), 2)
+cv2.drawContours(img_copy, rectangles, -1, (0,255,0), 3)
 cv2.imshow('img with contours', img_copy)
 cv2.waitKey(0)
+
+
+cv2.imwrite('detected_plates.jpg', img_copy)
